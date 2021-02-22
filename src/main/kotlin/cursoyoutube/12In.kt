b5703df3fd688a8d17cf279d8b19a8c591ddc255
amandalima <amandaisabelalima@gmail.com>
2021-02-21 21:53:43 -0300

    collections and in

--- src/main/kotlin/cursoyoutube/12In.kt
+++ src/main/kotlin/cursoyoutube/12In.kt
@@ -1,2 +1,12 @@
 package cursoyoutube
 
+fun main() {
+    println(isLetter('a'))
+    println(isLetter('9'))
+    println(isNotDigit('5'))
+    println(isNotDigit('f'))
+}
+
+fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+
+fun isNotDigit(c: Char) = c !in '0'..'9'
---
+++ src/main/kotlin/cursoyoutube/13Collections.kt
@@ -0,0 +1,26 @@
+package cursoyoutube
+
+fun main() {
+
+    //classe
+    val fruits = ArrayList<String>()
+    fruits.add("apple")
+    fruits.add("banana")
+
+    println(fruits)
+
+    //função
+    val itens = arrayListOf("apple", "banana")
+    println(itens)
+    println(itens.last())
+    println(itens.first())
+
+    //kotlin não tem as pŕoprias collections.. utiliza as do java
+    println(ArrayList::class.java)
+
+
+    val numbers = setOf(1, 14, 2)
+    println(numbers)
+    println(numbers.maxOrNull())
+
+}
